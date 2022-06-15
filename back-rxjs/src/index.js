3abe256e524479d9778e44bbd755a9d802f981b1
Danil Kravchenko <danil.k@qualer.com>
2022-06-15 23:05:42 +0300

    add rx-dom + js

--- back-rxjs/src/index.js
+++ back-rxjs/src/index.js
@@ -1,39 +1,9 @@
-import {EMPTY, fromEvent} from 'rxjs'
-import {map, debounceTime, distinctUntilChanged, switchMap, mergeMap, tap, catchError, filter} from 'rxjs/operators'
-import {ajax} from 'rxjs/ajax'
+import Rx from 'rx-dom'
 
-const url = 'https://api.github.com/search/users?q='
+var url = 'https://stream.wikimedia.org/v2/stream/recentchange';
 
+var source = Rx.DOM.fromEventSource(url);
 
-const search = document.getElementById('search')
-const result = document.getElementById('result')
-
-const stream$ = fromEvent(search, 'input')
-  .pipe(
-    map(e => e.target.value),
-    debounceTime(1000),
-    distinctUntilChanged(),
-    tap(() => result.innerHTML = ''),
-    filter(v => v.trim()),
-    switchMap(v => ajax.getJSON(url + v).pipe(
-      catchError(err => EMPTY)
-    )),
-    map(response => response.items),
-    mergeMap(items => items)
-  )
-
-
-stream$.subscribe(user => {
-  const html = `
-    <div class="card">
-      <div class="card-image">
-        <img src="${user.avatar_url}" />
-        <span class="card-title">${user.login}</span>
-      </div>
-      <div class="card-action">
-        <a href="${user.html_url}" target="_blank">Открыть github</a>
-      </div>
-    </div>
-  `
-  result.insertAdjacentHTML('beforeend', html)
-})
+source.subscribe(function (e) {
+  console.log('Received data: ' + e);
+});
