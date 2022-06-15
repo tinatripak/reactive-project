570b79f825d3dae2eac74e42144643d643eedae7
Danil Kravchenko <danil.k@qualer.com>
2022-06-15 23:26:22 +0300

    add observableEventSource

--- back-rxjs/src/index.js
+++ back-rxjs/src/index.js
@@ -1,9 +1,28 @@
-import Rx from 'rx-dom'
+import observableEventSource from 'observable-event-source';
 
 var url = 'https://stream.wikimedia.org/v2/stream/recentchange';
 
-var source = Rx.DOM.fromEventSource(url);
+const o = observableEventSource({
+  url: url,
+  json: true
+})
+ 
+console.log('recently changed npm packages...')
 
-source.subscribe(function (e) {
-  console.log('Received data: ' + e);
-});
+myMap(o).subscribe(response => {
+    console.log(response)
+    const html = `
+      <div style="float: left; margin-left: 10px;">${response.$schema}</div>
+      <div style="float: left; margin-left: 10px;">${response.id}</div>
+      <div style="float: left; margin-left: 10px;">${response.user}</div>
+    `
+    result.insertAdjacentHTML('beforeend', html)
+  })
+
+function myMap(o) {
+    o.map(function (response) {
+        return response
+      })
+
+    return o;
+}
