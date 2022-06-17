844d7bf87e8c0a6bfcc6b1f43802ce9fdce2cd64
Danil Kravchenko <danil.k@qualer.com>
2022-06-17 19:17:41 +0300

    add task 2

--- back-rxjs/src/recent_changes/index copy.js
+++ back-rxjs/src/recent_changes/index copy.js
@@ -6,29 +6,18 @@ const o = observableEventSource({
   url: url,
   json: true
 })
-
-var searchUsers = []
  
 console.log('recently changed npm packages...')
-const search = document.getElementById('search')
 
 var updateBuffer = makeDisplayBuffer(10);
 
-filter(o).subscribe(response => {
-  if (searchUsers.includes(response.user)) {
-    var node = document.createTextNode(response.title + '-' + response.user + '\n\n');
-    result.prepend(node);
-    updateBuffer(node);
-  }
+myMap(o).subscribe(response => {
+  var node = document.createTextNode(response.title + '-' + response.user + '\n\n');
+  result.prepend(node);
+  updateBuffer(node);
 })
 
-search.addEventListener('change', (event) => {
-  searchUsers = search.value.split('|')
-  console.log(searchUsers)
-});
-
-
-function filter(o) {
+function myMap(o) {
   o.map(function (response) {
     return response
   })
---
+++ back-rxjs/src/track-realtime_activity/index.js
@@ -0,0 +1,48 @@
+import observableEventSource from 'observable-event-source';
+
+var url = 'https://stream.wikimedia.org/v2/stream/recentchange';
+
+const o = observableEventSource({
+  url: url,
+  json: true
+})
+
+var searchUsers = []
+ 
+console.log('recently changed npm packages...')
+const search = document.getElementById('search')
+
+var updateBuffer = makeDisplayBuffer(10);
+
+filter(o).subscribe(response => {
+  if (searchUsers.includes(response.user)) {
+    var node = document.createTextNode(response.title + '-' + response.user + '\n\n');
+    result.prepend(node);
+    updateBuffer(node);
+  }
+})
+
+search.addEventListener('change', (event) => {
+  searchUsers = search.value.split('|')
+  console.log(searchUsers)
+});
+
+
+function filter(o) {
+  o.map(function (response) {
+    return response
+  })
+
+  return o;
+}
+
+function makeDisplayBuffer(size) {
+  var buffer = [];
+  return function (element) {
+    buffer.push(element);
+    if (buffer.length > size) {
+      var popped = buffer.shift();
+      popped.parentNode.removeChild(popped);
+    }
+  }
+}
