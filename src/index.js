e6777b03546aaac0e59936d75422d6ca6b644999
Roman <roman200220022002@gmail.com>
2022-06-18 14:31:32 +0300

    some lite changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import observableEventSource from 'observable-event-source';
-
+import { filter, makeDisplayBuffer } from "./track-realtime_activity/index.js";
 var url = 'https://stream.wikimedia.org/v2/stream/recentchange';
 
 const o = observableEventSource({
@@ -15,6 +15,7 @@ const search = document.getElementById('search')
 var updateBuffer = makeDisplayBuffer(16);
 
 filter(o).subscribe(response => {
+  //console.log(response);
   if (searchUsers.includes(response.user)) {
     var node = document.createTextNode(response.title + '-' + response.user + '\n\n');
     result.prepend(node);
@@ -26,23 +27,3 @@ search.addEventListener('change', (event) => {
   searchUsers = search.value.split('|')
   console.log(searchUsers)
 });
-
-
-function filter(o) {
-  o.map(function (response) {
-    return response
-  })
-
-  return o;
-}
-
-function makeDisplayBuffer(size) {
-  var buffer = [];
-  return function (element) {
-    buffer.push(element);
-    if (buffer.length > size) {
-      var popped = buffer.shift();
-      popped.parentNode.removeChild(popped);
-    }
-  }
-}
--- src/track-realtime_activity/index.js
+++ src/track-realtime_activity/index.js
@@ -1,34 +1,4 @@
-import observableEventSource from 'observable-event-source';
-
-var url = 'https://stream.wikimedia.org/v2/stream/recentchange';
-
-const o = observableEventSource({
-  url: url,
-  json: true
-})
-
-var searchUsers = []
- 
-console.log('recently changed npm packages...')
-const search = document.getElementById('search')
-
-var updateBuffer = makeDisplayBuffer(16);
-
-filter(o).subscribe(response => {
-  if (searchUsers.includes(response.user)) {
-    var node = document.createTextNode(response.title + '-' + response.user + '\n\n');
-    result.prepend(node);
-    updateBuffer(node);
-  }
-})
-
-search.addEventListener('change', (event) => {
-  searchUsers = search.value.split('|')
-  console.log(searchUsers)
-});
-
-
-function filter(o) {
+export function filter(o) {
   o.map(function (response) {
     return response
   })
@@ -36,7 +6,7 @@ function filter(o) {
   return o;
 }
 
-function makeDisplayBuffer(size) {
+export function makeDisplayBuffer(size) {
   var buffer = [];
   return function (element) {
     buffer.push(element);
